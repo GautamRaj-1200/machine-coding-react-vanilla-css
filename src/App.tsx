2e167611e55f4078ea6ac6868d8d84a39b68fb3f
GautamRaj-1200 <gautamrajv2@gmail.com>
2025-02-20 13:55:34 +0530

    Add handle click outside in Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import Autocomplete from "./components/Autocomplete/Autocomplete";
 import "./App.css";
+import Pagination from "./components/Pagination/Pagination";
 const App = () => {
   return (
     <>
       <div className="app">
         <Navbar />
         <Autocomplete />
+        <Pagination />
       </div>
     </>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./navbar.css";
 const Navbar = () => {
   const [navbarVisible, setNavbarVisible] = useState<boolean>(false);
+  const navbarRef = useRef<HTMLUListElement | null>(null);
+  const hamburgerRef = useRef<HTMLDivElement>(null);
 
   const toggleNavbar = () => {
     setNavbarVisible(!navbarVisible);
   };
   console.log(navbarVisible);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      navbarRef.current &&
+      !navbarRef.current.contains(event.target as Node) &&
+      hamburgerRef.current &&
+      !hamburgerRef.current.contains(event.target as Node)
+    ) {
+      setNavbarVisible(false);
+    }
+  };
+  const handleLinkClick = () => {
+    setNavbarVisible(false);
+  };
+
+  useEffect(() => {
+    if (navbarVisible)
+      document.addEventListener("mousedown", handleClickOutside);
+  }, [navbarVisible]);
+
   return (
     <>
       <header className="header">
@@ -14,35 +36,38 @@ const Navbar = () => {
           <div className="navbar__logo">
             <h1>Brand Logo</h1>
           </div>
-          <ul className={`navbar__list ${navbarVisible ? "active" : ""}`}>
+          <ul
+            ref={navbarRef}
+            className={`navbar__list ${navbarVisible ? "active" : ""}`}
+          >
             <li className="navbar__item">
-              <a href="#" className="navbar__link">
+              <a href="#" className="navbar__link" onClick={handleLinkClick}>
                 Home
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#" className="navbar__link">
+              <a href="#" className="navbar__link" onClick={handleLinkClick}>
                 About
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#" className="navbar__link">
+              <a href="#" className="navbar__link" onClick={handleLinkClick}>
                 Contact
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#" className="navbar__link">
+              <a href="#" className="navbar__link" onClick={handleLinkClick}>
                 Blogs
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#" className="navbar__link">
+              <a href="#" className="navbar__link" onClick={handleLinkClick}>
                 {" "}
                 SignUp
               </a>
             </li>
             <li className="navbar__item">
-              <a href="#" className="navbar__link">
+              <a href="#" className="navbar__link" onClick={handleLinkClick}>
                 {" "}
                 Login
               </a>
