0f7158fa0bebc644ff9e39e626afef0566b37a0d
GautamRaj-1200 <gautamrajv2@gmail.com>
2025-03-23 09:29:36 +0530

    Add global container

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import Navbar from "./components/Navbar/Navbar";
 import Autocomplete from "./components/Autocomplete/Autocomplete";
 import Pagination from "./components/Pagination/Pagination";
-import ThreeColumnGrid from "./components/ThreeColumnGrid/ThreeColumnGrid";
+import ThreeColumnGrid from "./components/ThreeColumnGridSimple/ThreeColumnGrid";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 const App = () => {
   return (
     <>
-      <div className="app">
-        <h3>
-          <Link to="/">Home</Link>
-        </h3>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/navbar" element={<Navbar />} />
-          <Route path="/autocomplete" element={<Autocomplete />} />
-          <Route path="/pagination" element={<Pagination />} />
-          <Route path="/three-column-grid" element={<ThreeColumnGrid />} />
-        </Routes>
+      <div className="app-wrapper">
+        <div className="content-container">
+          <h3>
+            <Link to="/">Home</Link>
+          </h3>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/navbar" element={<Navbar />} />
+            <Route path="/autocomplete" element={<Autocomplete />} />
+            <Route path="/pagination" element={<Pagination />} />
+            <Route path="/three-column-grid" element={<ThreeColumnGrid />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
---
+++ src/components/ThreeColumnGridSimple/ThreeColumnGrid.tsx
@@ -0,0 +1,20 @@
+import "./ThreeColumGridSimple.css";
+const ThreeColumnGridSimple = () => {
+  return (
+    <>
+      <section className="grid">
+        <div className="grid-container">
+          <div className="grid-item"></div>
+          <div className="grid-item"></div>
+          <div className="grid-item"></div>
+          <div className="grid-item"></div>
+          <div className="grid-item"></div>
+          <div className="grid-item"></div>
+          <div className="grid-item"></div>
+          <div className="grid-item"></div>
+        </div>
+      </section>
+    </>
+  );
+};
+export default ThreeColumnGridSimple;
